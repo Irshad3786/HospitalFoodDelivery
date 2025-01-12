ff0a34034a92a2524f933498d2e223851a6a9ab4
irshad3786 <irshad9182278505@gmail.com>
2025-01-12 23:56:30 +0530

    management account ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom'
+import Management from './pages/Management'
 
 function App() {
-
+  const Router = createBrowserRouter([
+    {
+      path:'/',
+      element : <Home/>
+    },
+    {
+      path:'/CreateAccount',
+      element : <Management/>
+    },
+    
+  ])
   return (
     <>
-      <Home/>
+      <RouterProvider router={Router}/>
     </>
   )
 }
