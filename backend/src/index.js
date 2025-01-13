1ee65cb5cdc9363f92702fa32aad23e5131b06d3
irshad3786 <irshad9182278505@gmail.com>
2025-01-14 01:16:36 +0530

    db add

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,5 +28,5 @@ mongoose.connect(process.env.MONGOURL)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on the http://localhost:${PORT}`);
 });
