c33925b182b1d428d0f8b1f6702e4468fd0d9fba
irshad3786 <irshad9182278505@gmail.com>
2025-01-15 00:08:06 +0530

    add text

--- frontend/src/pages/Management.jsx
+++ frontend/src/pages/Management.jsx
@@ -7,20 +7,20 @@ function Management() {
     <div className='w-[100%] h-screen bg-[#00FFAA]'>
 
         <div className='flex justify-center items-center p-8'>
-            <h1 className='font-roboto font-semibold text-3xl'>Create Account </h1>
+            <h1 className='font-roboto font-semibold text-3xl '>Create Your Food Manager Account</h1>
         </div>
         <div>
             <Form>
-                <div className='flex flex-col justify-center items-center p-4 gap-4'>
-                    <input type="text" placeholder='Full Name' className='p-3 rounded-md sm:w-[30%]' />
-                    <input type="email" placeholder='Email' className='p-3 rounded-md sm:w-[30%]' />
-                    <input type="text" placeholder='Phone number' className='p-3 rounded-md sm:w-[30%]'/>
-                    <input type="text" placeholder='Employe ID' className='p-3 rounded-md sm:w-[30%]'/>
-                    <input type="password" placeholder='Password' className='p-3 rounded-md sm:w-[30%]'/>
-                    <input type="password"  placeholder='Confirm Password' className='p-3 rounded-md sm:w-[30%]'/>
+                <div className='flex flex-col justify-center items-center p-4 gap-4 '>
+                    <input type="text" placeholder='Full Name' className='p-3 rounded-md sm:w-[30%] shadow-lg' />
+                    <input type="email" placeholder='Email' className='p-3 rounded-md sm:w-[30%] shadow-lg' />
+                    <input type="text" placeholder='Phone number' className='p-3 rounded-md sm:w-[30%] shadow-lg'/>
+                    <input type="text" placeholder='Employe ID' className='p-3 rounded-md sm:w-[30%] shadow-lg'/>
+                    <input type="password" placeholder='Password' className='p-3 rounded-md sm:w-[30%] shadow-lg' />
+                    <input type="password"  placeholder='Confirm Password' className='p-3 rounded-md sm:w-[30%] shadow-lg'/>
                 </div>
                 <div className='flex justify-center items-center p-4'>
-                    <button className='bg-white px-4 py-2 rounded-xl hover:bg-slate-400'>Submit</button>
+                    <button className='bg-white px-4 py-2 rounded-xl hover:bg-slate-400 font-roboto'>Submit</button>
                 </div>
             </Form>
         </div>
