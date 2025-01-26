4d903bef6f8444f809d34fbef4af4e9b246b1c7b
irshad3786 <irshad9182278505@gmail.com>
2025-01-26 23:57:03 +0530

    added dietLayout

--- frontend/src/pages/CreateDiet.jsx
+++ frontend/src/pages/CreateDiet.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function CreateDiet() {
   return (
-    <div>CreateDiet</div>
+    <div className='w-[100%] h-screen bg-[#00FFAA] flex justify-center items-center'>
+      <div className='w-[70%] bg-white rounded h-[80%] '>
+        <div>Morning</div>
+        <div>Patient details</div>
+        <div>Food items</div>
+        <div>ingredents</div>
+      </div>
+    </div>
   )
 }
 
